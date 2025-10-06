c648821e28983c4d5f64e4b4e1701b7bad061f68
Dan <dan@dtbmail.com>
2025-10-06 14:58:03 +0100

    Start

---
+++ src/notion_keeper.py
@@ -0,0 +1,7 @@
+class NotionKeeper:
+
+    def __init__(self, export_path: str, import_path: str) -> None:
+        self.export_path = export_path
+        self.import_path = import_path
+
+        # Initialize Logger
